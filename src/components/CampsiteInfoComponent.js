db7a0d5663009152ac63eb358c8969c79f9e0ac9
Stephanie Raymos <stephanieraymos@gmail.com>
2020-04-25 10:17:39 -0700

    Added CampsiteInfo Component

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class CampsiteInfo extends Component {
+  render() {
+    if (campsite != null){
+      return (
+        <div className="row">
+          
+        </div>
+      )
+    }else{
+      return (
+        <div>
+          
+        </div>
+      )
+    }
+
+  }
+}
+
+export default CampsiteInfo
