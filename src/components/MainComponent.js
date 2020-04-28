1e61ae1beb304f6875ba6f1bad1e874157e9725f
Stephanie Raymos <stephanieraymos@gmail.com>
2020-04-27 18:28:54 -0700

    Added filter method to CampsiteInfo to return an array with one object, extracted with [0]

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -26,7 +26,7 @@ class Main extends Component {
                 </div>
                 </Navbar>
                 <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={(this.state.selectedCampsite)} />
+                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
             </div>
         );
     }
