a6d3ce947f80b807f3a2959f347d6335a0fb484c
Stephanie Raymos <stephanieraymos@gmail.com>
2020-04-27 18:21:34 -0700

    Made Directory a presentational component and created Main as the container component

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -3,23 +3,11 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 import CampsiteInfo from './CampsiteInfoComponent';
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-//Deleted    renderSelectedCampsite(campsite)
-
     render() {
         const directory = this.props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card onClick={}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -34,10 +22,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                
-                        <CampsiteInfo campsite={(this.state.selectedCampsite)} />
-                   
-        
+          
             </div>
         );
     };
