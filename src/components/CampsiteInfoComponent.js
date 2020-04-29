4cd6f9e09c41137d79c067478e187c1d3298130d
Stephanie Raymos <stephanieraymos@gmail.com>
2020-04-29 15:56:32 -0700

    Split up class components into 3 functional components. Fixed bugs

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,35 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampsiteInfo extends Component {
-  constructor(props) {
-    super(props)
 
-    /*this.state = {
-      
-    };*/
-  }
-  render() {
-    if (this.props.campsite) {
-      return (
-        <div className="container">
-          <div className="row">
-            {this.renderCampsite(this.props.campsite)}
-            {this.renderComments(this.props.campsite.comments)}
-          </div>
-        </div>
-      )
-    } else {
-      return (
-        <div>
-
-        </div>
-      )
-    }
-
-  }
-
-  renderComments(comments) {
+  function RenderComments({comments}) {
     if (comments) {
       return (
         <div className="col-md-5 m-1">
@@ -51,7 +24,7 @@ class CampsiteInfo extends Component {
     )
   };
 
-  renderCampsite(campsite) {
+  function RenderCampsite({campsite}) {
     return (
       <div className="col-md-5 m-1">
         <Card>
@@ -64,7 +37,26 @@ class CampsiteInfo extends Component {
       </div>)
   };
 
-}
+  function CampsiteInfo(props) {
+    if (props.campsite) {
+      return (
+        <div className="container">
+          <div className="row">
+            <RenderCampsite campsite={props.campsite} />
+            <RenderComments comments= {props.campsite.comments} />
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+
+        </div>
+      )
+    }
+
+  }
+
 
 export default CampsiteInfo;
 
