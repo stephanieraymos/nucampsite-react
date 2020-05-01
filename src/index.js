37eb404fd8d6c1ae40784615ad668254ea049b66
Stephanie Raymos <stephanieraymos@gmail.com>
2020-04-30 18:25:43 -0700

    Installed Font awesome + Bootstrap social

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
