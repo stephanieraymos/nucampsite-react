de3c80ea23ce563f74d4a91777ac2adf6235df92
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-06 19:31:30 -0700

    Added login modal

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+  Button, Modal, ModalHeader, ModalBody,
+  Form, FormGroup, Input, Label } from 'reactstrap';import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
 
@@ -9,15 +10,27 @@ class Header extends Component {
 
     this.toggleNav = this.toggleNav.bind(this);
     this.state = {
-      isNavOpen: false
-    };
-  }
+      isNavOpen: false,
+      isModalOpen: false
+  };
+
+  this.toggleNav = this.toggleNav.bind(this);
+  this.toggleModal = this.toggleModal.bind(this);
+}
+  
 
   toggleNav() {
     this.setState({
       isNavOpen: !this.state.isNavOpen
     });
   }
+
+  toggleModal() {
+    this.setState({
+        isModalOpen: !this.state.isModalOpen
+    });
+}
+
 
   render() {
     return (
@@ -59,9 +72,22 @@ class Header extends Component {
                                   </NavLink>
                 </NavItem>
               </Nav>
+                            <span className="navbar-text ml-auto">
+                                <Button outline onClick={this.toggleModal}>
+                                    <i className="fa fa-sign-in fa-lg" /> Login
+                                </Button>
+                            </span>
             </Collapse>
           </div>
         </Navbar>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    
+                    </ModalBody>
+                </Modal>
+
       </React.Fragment>
     );
   }
