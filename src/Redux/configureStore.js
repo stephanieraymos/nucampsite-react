e9f43ea224f0d918a3c552fe7e24358e8bfbf4d5
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-08 18:30:17 -0700

    Fixed configure store folder path

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore } from './node_modules/redux';
+import { createStore } from '../../node_modules/redux';
 import { Reducer, initialState } from './reducer';
 
 export const ConfigureStore = () => {
