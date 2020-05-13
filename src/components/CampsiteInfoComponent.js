40a30d0eec00c688cd68c188229199457a613423
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-12 19:33:37 -0700

    Updated CampsiteInfoComponent

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Button, Modal, ModalHeader, ModalBody, Card, CardImg, CardText, CardBody, Label, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -129,6 +131,26 @@ function RenderCampsite({ campsite }) {
 };
 
 function CampsiteInfo(props) {
+  if (props.isLoading) {
+    return (
+        <div className="container">
+            <div className="row">
+                <Loading />
+            </div>
+        </div>
+    );
+}
+if (props.errMess) {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        </div>
+    );
+}
   if (props.campsite) {
     return (
       <div className="container">
