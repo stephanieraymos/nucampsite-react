2bee3c13fd297e9934dc46ae54689e60fa747963
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-12 19:34:48 -0700

    Updated Home Component

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
 
-function RenderCard({item}) {
+
+function RenderCard({item, isLoading, errMess}) {
+    if (isLoading) {
+        return (
+             <Loading />
+        );
+    }
+    if (errMess) {
+        return (
+            <h4>{errMess}</h4>
+        );
+    }
     return (
-        <Card className="h-100">
+        <Card>
             <CardImg src={item.image} alt={item.name} />
             <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
+            <CardTitle>{item.name}</CardTitle>
+            <CardText>{item.description}</CardText>
             </CardBody>
         </Card>
     );
@@ -19,7 +31,11 @@ function Home(props) {
         <div className="container">
             <div className="row">
                 <div className="col-md m-1">
-                    <RenderCard item={props.campsite} />
+                <RenderCard
+                        item={props.campsite}
+                        isLoading={props.campsitesLoading}
+                        errMess={props.campsitesErrMess}
+                    />
                 </div>
                 <div className="col-md m-1">
                     <RenderCard item={props.promotion} />
