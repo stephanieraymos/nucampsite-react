05a7f304ce0310fe9e1fe5546837b3b69438a675
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-14 21:02:04 -0700

    Fixed bugs

--- src/Redux/ActionCreators.js
+++ src/Redux/ActionCreators.js
@@ -11,29 +11,37 @@ export const addComment = (campsiteId, rating, author, text) => ({
     }
 });
 
+export const commentsFailed = errMess => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errMess
+});
+export const addComments = comments => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
 
+//CAMPSITES 
 export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
 
     return fetch(baseUrl + 'campsites')
-    .then(response => {
+        .then(response => {
             if (response.ok) {
-                 return response;
+                return response;
             } else {
-                const error = new Error(`Error ${response.status}: ${response.statusText}`);                error.response = response;
+                const error = new Error(`Error ${response.status}: ${response.statusText}`); error.response = response;
                 throw error;
             }
         },
-        error => {
-            const errMess = new Error(error.message);
-            throw errMess;
-        }
-    )
-    .then(response => response.json())
-    .then(campsites => dispatch(addCampsites(campsites)))
-    .catch(error => dispatch(campsitesFailed(error.message)));
+            error => {
+                const errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(campsites => dispatch(addCampsites(campsites)))
+        .catch(error => dispatch(campsitesFailed(error.message)));
 };
-
 
 
 export const campsitesLoading = () => ({
@@ -50,52 +58,42 @@ export const addCampsites = campsites => ({
     payload: campsites
 });
 
-
-
-
-
-
-export const fetchComments = () => dispatch => {    
+//FETCH COMMENTS
+export const fetchComments = () => dispatch => {
     return fetch(baseUrl + 'comments')
-    .then(response => {
+        .then(response => {
             if (response.ok) {
                 return response;
             } else {
-                const error = new Error(`Error ${response.status}: ${response.statusText}`);                error.response = response;
+                const error = new Error(`Error ${response.status}: ${response.statusText}`); error.response = response;
                 throw error;
             }
         },
-        error => {
-            var errMess = new Error(error.message);
-            throw errMess;
-        }
-    )
-    .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)))
-    .catch(error => dispatch(commentsFailed(error.message)));
+            error => {
+                var errMess = new Error(error.message);
+                throw errMess;
+            }
+        )
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
 };
 
-
-
-
-
-
-
-
+///PROMOTIONS 
 export const fetchPromotions = () => (dispatch) => {
-    
+
     dispatch(promotionsLoading());
 
     return fetch(baseUrl + 'promotions')
         .then(response => {
-                if (response.ok) {
-                    return response;
-                } else {
-                    const error = new Error(`Error ${response.status}: ${response.statusText}`);
-                    error.response = response;
-                    throw error;
-                }
-            },
+            if (response.ok) {
+                return response;
+            } else {
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
             error => {
                 const errMess = new Error(error.message);
                 throw errMess;
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-
+import { Partners } from '../redux/partners';
 
 
 function RenderCard({item, isLoading, errMess}) {
